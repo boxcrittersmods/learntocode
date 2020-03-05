e00180879f224f5c553ffc24ca785a8e7faedaf5
Tumble <tumblegamer@gmail.com>
2020-03-05 17:29:56 +0000

    added blcoks

--- ltc/block-defs.js
+++ ltc/block-defs.js
@@ -1,56 +1,170 @@
-Blockly.JavaScript['say'] = function(block) {
-  var value_message = Blockly.JavaScript.valueToCode(block, 'MESSAGE', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript["say"] = function(block) {
+  var value_message = Blockly.JavaScript.valueToCode(
+    block,
+    "MESSAGE",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
   // TODO: Assemble JavaScript into code variable.
   var code = `BoxCritters.Say(${value_message});\n`;
   return code;
 };
 
-Blockly.JavaScript['pos'] = function(block) {
-  var value_x = Blockly.JavaScript.valueToCode(block, 'X', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_y = Blockly.JavaScript.valueToCode(block, 'Y', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript["pos"] = function(block) {
+  var value_x = Blockly.JavaScript.valueToCode(
+    block,
+    "X",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var value_y = Blockly.JavaScript.valueToCode(
+    block,
+    "Y",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
   // TODO: Assemble JavaScript into code variable.
   var code = `{x:${value_x},y:${value_y}}`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['goto'] = function(block) {
-  var value_pos = Blockly.JavaScript.valueToCode(block, 'POS', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript["goto"] = function(block) {
+  var value_pos = Blockly.JavaScript.valueToCode(
+    block,
+    "POS",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
   // TODO: Assemble JavaScript into code variable.
   var code = `BoxCritters.Goto(${value_pos});\n`;
   return code;
 };
 
-Blockly.JavaScript['player_pos'] = function(block) {
+Blockly.JavaScript["player_pos"] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = 'BoxCritters.PlayerPos()';
+  var code = "BoxCritters.PlayerPos()";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['pos_x'] = function(block) {
-  var value_pos = Blockly.JavaScript.valueToCode(block, 'POS', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript["pos_x"] = function(block) {
+  var value_pos = Blockly.JavaScript.valueToCode(
+    block,
+    "POS",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
   // TODO: Assemble JavaScript into code variable.
   var code = `${value_pos}.x`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['pos_y'] = function(block) {
-  var value_pos = Blockly.JavaScript.valueToCode(block, 'POS', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript["pos_y"] = function(block) {
+  var value_pos = Blockly.JavaScript.valueToCode(
+    block,
+    "POS",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
   // TODO: Assemble JavaScript into code variable.
   var code = `${value_pos}.y`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-
-Blockly.JavaScript['repeat_forver'] = function(block) {
-  var statements_repeat_do = Blockly.JavaScript.statementToCode(block, 'REPEAT_DO');
+Blockly.JavaScript["repeat_forver"] = function(block) {
+  var statements_repeat_do = Blockly.JavaScript.statementToCode(
+    block,
+    "REPEAT_DO"
+  );
   // TODO: Assemble JavaScript into code variable.
-  var code = [
-  'while (true) {',
-  `  ${statements_repeat_do}`,
-  '}'].join("\n") + "\n";
+  var code = `
+setInterval(function() {
+	${statements_repeat_do}
+},1000/30)`;
   return code;
 };
+
+Blockly.JavaScript["var"] = function(block) {
+  var value_code = Blockly.JavaScript.valueToCode(
+    block,
+    "CODE",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  // TODO: Assemble JavaScript into code variable.
+  var code = `${value_code}`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript["run_return"] = function(block) {
+  var value_function = Blockly.JavaScript.valueToCode(
+    block,
+    "FUNCTION",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  // TODO: Assemble JavaScript into code variable.
+  var code = `${value_code}();`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript["run"] = function(block) {
+  var value_function = Blockly.JavaScript.valueToCode(
+    block,
+    "FUNCTION",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  // TODO: Assemble JavaScript into code variable.
+  var code = `${value_code}();`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript["return"] = function(block) {
+  var value_return = Blockly.JavaScript.valueToCode(
+    block,
+    "RETURN",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  // TODO: Assemble JavaScript into code variable.
+  var code = `return ${value_return};\n`;
+  return code;
+};
+
+Blockly.JavaScript["function"] = function(block) {
+  var statements_code = Blockly.JavaScript.statementToCode(block, "CODE");
+  // TODO: Assemble JavaScript into code variable.
+  var code = `function() {
+		${statements_code}
+	}`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript["object"] = function(block) {
+  var statements_properties = Blockly.JavaScript.statementToCode(
+    block,
+    "PROPERTIES"
+  );
+  // TODO: Assemble JavaScript into code variable.
+  var code = `
+	{
+		${statements_properties}
+	}
+	`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+Blockly.JavaScript['object_property'] = function(block) {
+	var text_key = block.getFieldValue('KEY');
+	var value_value = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
+	// TODO: Assemble JavaScript into code variable.
+	var code = `"${text_key}":${value_value}`,;
+	return code;
+  };
+
+  Blockly.JavaScript['get_prop'] = function(block) {
+	var text_prop = block.getFieldValue('PROP');
+	var value_next = Blockly.JavaScript.valueToCode(block, 'NEXT', Blockly.JavaScript.ORDER_ATOMIC);
+	// TODO: Assemble JavaScript into code variable.
+	var code = `.${text_prop}${value_next}`;
+	// TODO: Change ORDER_NONE to the correct strength.
+	return [code, Blockly.JavaScript.ORDER_NONE];
+  };
--- ltc/blocks.js
+++ ltc/blocks.js
@@ -1,92 +1,197 @@
 Blockly.Blocks['say'] = {
-  init: function() {
-    this.appendValueInput("MESSAGE")
-        .setCheck(null)
-        .appendField("say");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(160);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendValueInput("MESSAGE")
+			.setCheck(null)
+			.appendField("say");
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(160);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
 
 Blockly.Blocks['pos'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("pos");
-    this.appendValueInput("X")
-        .setCheck("Number");
-    this.appendValueInput("Y")
-        .setCheck("Number");
-    this.setInputsInline(true);
-    this.setOutput(true, "pos");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendDummyInput()
+			.appendField("pos");
+		this.appendValueInput("X")
+			.setCheck("Number");
+		this.appendValueInput("Y")
+			.setCheck("Number");
+		this.setInputsInline(true);
+		this.setOutput(true, "pos");
+		this.setColour(230);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
 
 Blockly.Blocks['goto'] = {
-  init: function() {
-    this.appendValueInput("POS")
-        .setCheck("pos")
-        .appendField("goto");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendValueInput("POS")
+			.setCheck("pos")
+			.appendField("goto");
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(230);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
 
 Blockly.Blocks['player_pos'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("player position");
-    this.setOutput(true, "pos");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendDummyInput()
+			.appendField("player position");
+		this.setOutput(true, "pos");
+		this.setColour(230);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
 
 Blockly.Blocks['pos_x'] = {
-  init: function() {
-    this.appendValueInput("POS")
-        .setCheck("pos");
-    this.appendDummyInput()
-        .appendField("x");
-    this.setOutput(true, "Number");
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendValueInput("POS")
+			.setCheck("pos");
+		this.appendDummyInput()
+			.appendField("x");
+		this.setOutput(true, "Number");
+		this.setColour(230);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
 
 Blockly.Blocks['pos_y'] = {
-  init: function() {
-    this.appendValueInput("POS")
-        .setCheck("pos");
-    this.appendDummyInput()
-        .appendField("y");
-    this.setOutput(true, "Number");
-    this.setColour(210);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendValueInput("POS")
+			.setCheck("pos");
+		this.appendDummyInput()
+			.appendField("y");
+		this.setOutput(true, "Number");
+		this.setColour(210);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
 
 Blockly.Blocks['repeat_forver'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("repeat forever");
-    this.appendStatementInput("REPEAT_DO")
-        .setCheck(null)
-        .appendField("do");
-    this.setPreviousStatement(true, null);
-    this.setColour(120);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
+	init: function () {
+		this.appendDummyInput()
+			.appendField("repeat forever");
+		this.appendStatementInput("REPEAT_DO")
+			.setCheck(null)
+			.appendField("do");
+		this.setPreviousStatement(true, null);
+		this.setColour(120);
+		this.setTooltip("");
+		this.setHelpUrl("");
+	}
 };
+
+Blockly.Blocks['run'] = {
+	init: function() {
+	  this.appendValueInput("CODE")
+		  .setCheck("function")
+		  .appendField(new Blockly.FieldLabelSerializable("run"), "CODE");
+	  this.setPreviousStatement(true, null);
+	  this.setNextStatement(true, null);
+	  this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+  
+  Blockly.Blocks['run_return'] = {
+	init: function() {
+	  this.appendValueInput("FUNCTION")
+		  .setCheck(null)
+		  .appendField("run");
+	  this.setOutput(true, null);
+	  this.setColour(260);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+  
+  Blockly.Blocks['var'] = {
+	init: function() {
+	  this.appendValueInput("CODE")
+		  .setCheck("String")
+		  .appendField(new Blockly.FieldLabelSerializable("var"), "CODE");
+	  this.setOutput(true, null);
+	  this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+  
+  Blockly.Blocks['return'] = {
+	init: function() {
+	  this.appendValueInput("RETURN")
+		  .setCheck(null)
+		  .appendField("return");
+	  this.setPreviousStatement(true, null);
+	  this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+
+  Blockly.Blocks['function'] = {
+	init: function() {
+	  this.appendDummyInput()
+		  .appendField("function");
+	  this.appendStatementInput("CODE")
+		  .setCheck(null)
+		  .setAlign(Blockly.ALIGN_CENTRE);
+	  this.setOutput(true, "function");
+	  this.setColour(260);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+
+  Blockly.Blocks['object'] = {
+	init: function() {
+	  this.appendDummyInput()
+		  .appendField("object");
+	  this.appendStatementInput("PROPERTIES")
+		  .setCheck("obj_prop");
+	  this.setOutput(true, null);
+	  this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+
+  Blockly.Blocks['object_property'] = {
+	init: function() {
+	  this.appendDummyInput()
+		  .appendField(new Blockly.FieldTextInput("default"), "KEY");
+	  this.appendValueInput("VALUE")
+		  .setCheck(null);
+	  this.setInputsInline(true);
+	  this.setPreviousStatement(true, "obj_prop");
+	  this.setNextStatement(true, "obj_prop");
+	  this.setColour(20);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
+
+
+  Blockly.Blocks['get_prop'] = {
+	init: function() {
+	  this.appendValueInput("NEXT")
+		  .setCheck("String")
+		  .appendField(".")
+		  .appendField(new Blockly.FieldTextInput("property"), "PROP");
+	  this.setOutput(true, null);
+	  this.setColour(160);
+   this.setTooltip("");
+   this.setHelpUrl("");
+	}
+  };
